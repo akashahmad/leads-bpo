c6b0d3eff95853b4a698bf3f6a4cc77b41a25e94
akashahmad <engr.akashahmad@gmail.com>
2019-11-17 19:21:14 +0500

    new page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from '../src/container/home/home'
-import Form from '../src/container/form/form'
 function App() {
   return (
     <>
-   <Form/>
-    {/* <Home /> */}
+     <Home />
     </>
   );
 }
--- src/container/form/form.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Form from '../../component/formComponents/form/form'
-export default ()=>{
-    return(
-        <>
-        <Form/>
-        </>
-    );
-}
