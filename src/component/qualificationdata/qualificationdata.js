1457263c13c62300ce331bd9a631d594b2b98840
akashahmad <engr.akashahmad@gmail.com>
2019-11-30 19:53:42 +0500

    phone number validation added

--- src/component/qualificationdata/qualificationdata.js
+++ src/component/qualificationdata/qualificationdata.js
@@ -127,7 +127,6 @@ export default () => {
     };
 
 
-
     const validateEmail = email => {
         let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(String(email).toLowerCase());
@@ -181,7 +180,7 @@ export default () => {
                                 newMessate[index] = {question: "submit"};
                             }
                             setShowQuestions(newMessate);
-                        }, 3000);
+                        }, 200);
                         return (
                             <div className="ml-5 mt-5 mb-3">
                                 <div className="d-flex Loader">
@@ -198,8 +197,8 @@ export default () => {
                         return (
                             <div className="chat1 mb-1">
                                 <div className="chat1-inside-data">
-                                    <div style={{marginLeft: "20px",marginRight: "70px",marginTop: "15px"}}>
-                                        <p id="input1-p"  className="ml-4 p">{single.question}</p>
+                                    <div style={{marginLeft: "20px", marginRight: "70px", marginTop: "15px"}}>
+                                        <p id="input1-p" className="ml-4 p">{single.question}</p>
                                     </div>
                                 </div>
                             </div>
@@ -208,106 +207,76 @@ export default () => {
                         return (
                             <div className="chat-input">
                                 <div className="chat-input-data">
-                                    <form className="d-flex" style={{alignItems:"flex-start",
-                                        alignContent:"Start",justifyContent:"end",minWidth:"86%"
+                                    <form className="d-flex" style={{
+                                        alignItems: "flex-start",
+                                        alignContent: "Start", justifyContent: "end", minWidth: "86%"
                                     }}
-                                          onSubmit={(event) =>
-                                          {
-                                            event.preventDefault();
-                                             if (single.type === "number") {
-                                                 {
-                                                     if (!phoneNumberValidator("+" + showQuestions[index].value)) {
-                                                                 let newMessate = [...showQuestions];
-                                                                 newMessate[index].message = " * Enter a valid Pattern i.e" +
-                                                                     "0044 7804944083"
-                                                                 setShowQuestions(newMessate);
-
-                                                             }
-                                                     else {
-                                                                 if (!checkValidaty) {
-                                                                     let newMessate1 = [...showQuestions];
-                                                                     newMessate1[index].message = " * Request failed with status code 509";
-                                                                     setShowQuestions(newMessate1);
-
-
-                                                                 }
-                                                                 else {
-
-                                                                     const word = showQuestions[index].value;
-                                                                     const newnumber = word.substring(word.length - 10);
-                                                                     const  validnumber="0"+newnumber;
-
-                                                                     let payLoad = {phone: validnumber};
-                                                                     axios.post(PhonevalidationapiPath+ "/api/phoneVerification", payLoad)
-                                                                         .then(response => {
-                                                                             let newMessate = [...showQuestions];
-                                                                             newMessate[index].disable = true;
-                                                                             newMessate[index].value = "+" + showQuestions[index].value;
-                                                                             newMessate.push({question: "typing"});
-                                                                             setShowQuestions(newMessate);
-
-                                                                         })
-                                                                         .catch(err => {
-                                                                             let newMessate1 = [...showQuestions];
-                                                                             newMessate1[index].message = "Request failed with status code 509";
-                                                                             setShowQuestions(newMessate1);
-
-                                                                             //setError(err.message);
-                                                                            // return
-                                                                         });
-
-
-
-
-
-                                                                 }
-                                                             }
-                                                 }
-                                             }
-                                        else if (single.type === "email") {
-                                            console.log(showQuestions[index].value);
-                                            if (!validateEmail(showQuestions[index].value)) {
-                                                let newMessate = [...showQuestions];
-                                                newMessate[index].message = "Email is invalid";
-                                                setShowQuestions(newMessate);
-                                            }
-                                            else {
-                                                let newMessate = [...showQuestions];
-                                                newMessate[index].disable = true;
-                                                newMessate[index].value = showQuestions[index].value;
-                                                newMessate.push({question: "typing"});
-                                                setShowQuestions(newMessate);
-                                            }
-                                        }
-                                        else {
-                                            let newMessate = [...showQuestions];
-                                            newMessate[index].disable = true;
-                                            newMessate.push({question: "typing"});
-                                            setShowQuestions(newMessate);
-                                        }
-                                    }}>
+                                          onSubmit={(event) => {
+                                              event.preventDefault();
+                                              if (single.type === "number") {
+                                                  {
+                                                      const validnumber = showQuestions[index].value;
+                                                      let payLoad = {phone: validnumber};
+                                                      axios.post(PhonevalidationapiPath + "/api/phoneVerification", payLoad)
+                                                          .then(response => {
+                                                              let newMessate = [...showQuestions];
+                                                              newMessate[index].disable = true;
+                                                              newMessate.push({question: "typing"});
+                                                              setShowQuestions(newMessate);
+                                                          })
+                                                          .catch(err => {
+                                                              let newMessate = [...showQuestions];
+                                                              newMessate[index].message = "Phone Number is invalid";
+                                                              setShowQuestions(newMessate);
+                                                          });
+                                                  }
+                                              }
+                                              else if (single.type === "email") {
+                                                  console.log(showQuestions[index].value);
+                                                  if (!validateEmail(showQuestions[index].value)) {
+                                                      let newMessate = [...showQuestions];
+                                                      newMessate[index].message = "Email is invalid";
+                                                      setShowQuestions(newMessate);
+                                                  }
+                                                  else {
+                                                      let newMessate = [...showQuestions];
+                                                      newMessate[index].disable = true;
+                                                      newMessate[index].value = showQuestions[index].value;
+                                                      newMessate.push({question: "typing"});
+                                                      setShowQuestions(newMessate);
+                                                  }
+                                              }
+                                              else {
+                                                  let newMessate = [...showQuestions];
+                                                  newMessate[index].disable = true;
+                                                  newMessate.push({question: "typing"});
+                                                  setShowQuestions(newMessate);
+                                              }
+                                          }}>
                                         <div className="set-input" style={{
                                             marginLeft: "37px",
                                             padding: 0,
                                             width: "209px",
                                             marginTop: "42px"
                                         }}>
-                                            <p className=" p " style={{color: "white",width:"300px"}}>{single.question}</p>
+                                            <p className=" p "
+                                               style={{color: "white", width: "300px"}}>{single.question}</p>
 
                                         </div>
                                         {single.type === "number" ?
                                             <div>
-                                                <input  placeholder="" className="input ml-4"
+                                                <input placeholder="" className="input ml-4"
                                                        type="number"
                                                        required={true}
                                                        autoFocus
+                                                       disabled={showQuestions[index].disable}
                                                        value={showQuestions[index] &&
                                                        showQuestions[index].value ? showQuestions[index].value : null}
                                                        onChange={event => {
                                                            let newMessate = [...showQuestions];
                                                            let value = event.target.value;
                                                            newMessate[index].message = "";
-                                                           if (value.length <=14) {
+                                                           if (value.length <= 11) {
                                                                newMessate[index].value = value;
                                                            }
                                                            setShowQuestions(newMessate);
@@ -319,6 +288,7 @@ export default () => {
                                                 <input placeholder="" className="input mt-4 ml-4"
                                                        type="email"
                                                        required={true}
+                                                       disabled={showQuestions[index].disable}
                                                        autoFocus
                                                        value={showQuestions[index] && showQuestions[index].value ? showQuestions[index].value : null}
                                                        onChange={event => {
@@ -334,6 +304,7 @@ export default () => {
                                                 <input placeholder="" className="input  ml-4"
                                                        type={single.type}
                                                        required={true}
+                                                       disabled={showQuestions[index].disable}
                                                        minLength={2}
                                                        maxLength={16}
                                                        autoFocus
@@ -342,8 +313,10 @@ export default () => {
                                                            newMessate[index].value = event.target.value;
                                                            setShowQuestions(newMessate);
                                                        }}/>}
-                                        <div><Button style={{marginTop: "20%", marginLeft: "8%!important",
-                                            backgroundColor:"#035F80"}}
+                                        <div><Button style={{
+                                            marginTop: "20%", marginLeft: "8%!important",
+                                            backgroundColor: "#035F80"
+                                        }}
                                                      disabled={showQuestions[index].disable} type="submit"
                                                      variant="secondary" className="button ml-2">
                                             Go
@@ -360,14 +333,15 @@ export default () => {
                                     <div style={{marginTop: "49px", marginLeft: "34px"}}>
                                         <p className="sub-input" style={{color: "white"}}>
                                             I accept the<Link className="link"
-                                            to={'/privacy-policy'} target="_blank" style={{color: "#F37F00"}}>
+                                                              to={'/privacy-policy'} target="_blank"
+                                                              style={{color: "#F37F00"}}>
                                             &nbsp;Privacy Policy </Link>
                                             &nbsp;and
                                             <Link className="link"
-                                                to={'/privacy-policy'}
+                                                  to={'/privacy-policy'}
                                                   target="_blank"
                                                   style={{color: "#F37F00"}}>
-                                                 &nbsp; Terms & Conditions.</Link></p></div>
+                                                &nbsp; Terms & Conditions.</Link></p></div>
                                     <div style={{marginLeft: ""}}>
                                         <Button
                                             style={{padding: "16px", borderRadius: "11px"}}
